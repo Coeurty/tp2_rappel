f99779cef145ab57f1acd385fab2f760e5be5c3d
Dany Rose <danyrose1995@gmail.com>
2024-11-29 15:13:35 +0100

    feat: style

--- src/Form/ReminderType.php
+++ src/Form/ReminderType.php
@@ -6,8 +6,7 @@ use App\Entity\Category;
 use App\Entity\Reminder;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -16,7 +15,7 @@ class ReminderType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title', TextType::class, [
+            ->add('title', TextareaType::class, [
                 'label' => 'Titre',
             ])
             ->add('eventDate', null, [
